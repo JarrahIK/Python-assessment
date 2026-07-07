d1206734495b1116dc7315d23ea315ff21567a78
Ibraheem aljarrah <ikaljarrah23@cit.just.edu.jo>
2026-07-07 11:01:45 +0300

    p2

--- problem-2/Problem-2.py
+++ problem-2/Problem-2.py
@@ -1,1 +1,24 @@
-print("this is problem-2")
+employees = [
+    {"name":"Ali","salary":900},
+    {"name":"Sara","salary":1400},
+    {"name":"Ahmad","salary":2100},
+    {"name":"Lina","salary":1500}
+]
+def find_max_salary (employees:list):
+    max=-999
+    for employee in range(len(employees)):
+        if employees[employee]["salary"]>max:
+         max=employees[employee]["salary"]
+    print(max)
+##to find the max salary
+find_max_salary(employees)
+
+def find_avg_salary (employees:list):
+    sum=0
+    for employee in range(len(employees)):
+        sum+=employees[employee]["salary"]
+    print(sum/len(employees))
+
+
+##to find the avg salary
+find_avg_salary(employees)
